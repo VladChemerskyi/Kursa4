39f3df0fcfc9b1f7855e078a1e3556629b5598aa
Vlad Chemerskyi <34398756+VladChemerskyi@users.noreply.github.com>
2020-03-27 18:40:14 +0200

    Update Kursa4.py

--- Kursa4.py
+++ Kursa4.py
@@ -3,22 +3,32 @@ from tkinter import font as tkFont
 def func():
     pass
     
-def create_window():
+def create_window_about_us():
     window1 = Toplevel(window)
+    window1.geometry('400x100')
+    w = Label(window1, text="Just about us" ,font=helv36)
+    w.pack()
+    button_quit = Button(window1, width = 20, text="Quit", command=window1.destroy , font=helv35)
+    button_quit.place(relx=0.5, rely=0.88, anchor=CENTER)
+
+
     
 window = Tk()
+
 window.configure(background="green")
 window.title("TIMSnNM")
-window.geometry('1050x1000')
+window.geometry('800x500')
 window.resizable(False,False)
-helv36 = tkFont.Font(family='Helvetica', size=36, weight=tkFont.BOLD)
+helv36 = tkFont.Font(family='Helvetica', size=24, weight=tkFont.BOLD)
+helv35 = tkFont.Font(family='Helvetica', size=10, weight=tkFont.BOLD)
+
 
 
 button_continuous = Button(window, width = 20, text="Continuous", command=func, font=helv36)
 button_discrete = Button(window, width = 20, text="Discrete", command=func , font=helv36)
 button_methog_simple_iteration = Button(window, width = 20, text="Methon simple iteration", command=func , font=helv36 )
 button_newton_zeydel_iteration = Button(window, width = 20, text="Newton-Zeydel iteration", command=func, font=helv36)
-button_about_us = Button(window, text="About us", width = 20, command=create_window , font=helv36)
+button_about_us = Button(window, text="About us", width = 20, command=create_window_about_us , font=helv36)
 button_quit = Button(window, width = 20, text="Quit", command=window.destroy , font=helv36)
 
 button_continuous.place(relx=0.5, rely=0.08, anchor=CENTER)
